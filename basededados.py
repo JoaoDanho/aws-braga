d66b78b4d20bd2098ad2649a47da78ee121991b0
João Francisco Danho Rodrigues Martins Vieira <146037541+JoaoDanho@users.noreply.github.com>
2025-10-28 11:37:36 +0000

    Create basededados.py

---
+++ basededados.py
@@ -0,0 +1,11 @@
+# Dados
+import sys
+
+
+servidor = sys.argv[1]
+user = sys.argv[2]
+password = "******" 
+
+print(f"o servidor é {servidor}")
+print(f"o user chama-se {user}")
+print(f"a password é {password}")
